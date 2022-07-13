210cab8d673a444780866808513437b98fe671c5
Ben <59037332+bensuwun@users.noreply.github.com>
2022-07-13 10:14:59 +0800

    Update ScaleCanvas.lua

--- GUI/ScaleCanvas.lua
+++ GUI/ScaleCanvas.lua
@@ -15,3 +15,17 @@ scrollFrame.ChildAdded:Connect(function()
   
   -- GridLayout: scrollFrame.CanvasSize = UDim2.new(0,0,0,gridLayout.AbsoluteContentSize.Y)
 end)
+
+-------------------------[[ V2. OnPropertyChange ]]-------------------------------
+local gridLayout = script.Parent:WaitForChild("UIGridLayout")
+local scrollFrame = script.Parent
+
+local absoluteSize = gridLayout.AbsoluteContentSize
+scrollFrame.CanvasSize = UDim2.new(0, absoluteSize.X, 0, absoluteSize.Y + 20)
+
+local function onContentSizeChanged()
+	local absoluteSize = gridLayout.AbsoluteContentSize
+	scrollFrame.CanvasSize = UDim2.new(0, absoluteSize.X, 0 , absoluteSize.Y + 20)
+end
+
+gridLayout:GetPropertyChangedSignal("AbsoluteContentSize"):Connect(onContentSizeChanged)
